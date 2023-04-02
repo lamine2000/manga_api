2ce6e87081c78f58b359c92ec1d3a8c5660bf36d
lamine <lemzogueye.lg72@gmail.com>
2023-04-02 00:47:35 +0000

    Rewrote the webscrapping script
    - Avoided having malformed links in the chapter's 'pages' field
    -Provided a better structure for the database json file
    -Added a field named 'number' in each chapter document (this helps us do more efficient searches through the builtin Tinydb methods).
    -Removed the number of the chapter from it's title, since wes dedicated a field for that

--- one_piece_scrapper.py
+++ one_piece_scrapper.py
@@ -1,10 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
+import re
 
 db = TinyDB('one_piece_db.json', indent=4, ensure_ascii=False)
 INFORMATION = db.table('informations')
-MANGA = db.table('pages')
+MANGA = db.table('chapters')
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36 Edg/105.0.1343.33'
@@ -24,13 +25,13 @@ def get_informations(url: str):
     INFORMATION.insert({'author': author[2].text.replace('\n', ''),
                         'resume': resume.text,
                         'release-date': release_date[3].text,
-                        'category': [
-                            category[4].text]})
+                        'categories':
+                            category[4].text.replace('\n', '').split(', ')})
 
 
 get_informations('https://www.scan-vf.net/one_piece')
 
-CHAPTERS = []
+CHAPTERS = {}
 
 
 def get_all_chapters(url: str):
@@ -42,32 +43,32 @@ def get_all_chapters(url: str):
 
     for src in all_chapters:
         links = src['href']
-        CHAPTERS.append(links)
-    return CHAPTERS
-
-
-get_all_chapters('https://www.scan-vf.net/one_piece')
-
-
-def get_scan_names():
-    for url in CHAPTERS:
-        response = requests.get(url, headers=headers)
+        response = requests.get(links, headers=headers)
         soup = BeautifulSoup(response.content, 'html.parser')
-        # get names
-        names_class = soup.findAll('div', class_='page-header')
-        NAMES = []
-        for name in names_class:
-            name_extracted = name.find('b').text
-            NAMES.append(name_extracted)
+
+        # get chapter number and name from page header
+        chapter_name = soup.find('div', class_='page-header').find('b').text
+        match = re.search(r'#(\d+)\s*:\s*(.*)', chapter_name)
+        if match:
+            chapter_number = match.group(1)
+            chapter_name = match.group(2)
+        else:
+            chapter_number = None
 
         # get images
         images_class = soup.findAll('img', class_='img-responsive')
         IMAGES = []
         for img_src in images_class:
             image_extracted = img_src.get('data-src')
-            IMAGES.append(image_extracted)
-            print(f'Extraction de tous les images de {image_extracted} ...')
-        MANGA.insert({'name': NAMES, 'chapters': IMAGES})
 
+            IMAGES.append(image_extracted) if image_extracted is not None\
+                else print('')
 
-get_scan_names()
+            if image_extracted is not None:
+                print(f'\rExtraction de l\'image de {image_extracted} ...')
+
+        id_doc = MANGA.insert({'number': chapter_number, 'name': chapter_name, 'pages': IMAGES})
+        chapter = MANGA.get(doc_id=id_doc)
+        MANGA.update({'id': chapter_number}, Query().id == chapter.doc_id and Query().id != chapter_number)
+
+get_all_chapters('https://www.scan-vf.net/one_piece')

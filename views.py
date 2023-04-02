7080ad48243dd97e53edee615b375acf3bfe4fa4
lamine <lemzogueye.lg72@gmail.com>
2023-04-02 00:58:27 +0000

    Enhanced code quality:
    -Removed unnecessary lines and variables for better lisibility
    -Used builtin query methods because of their optimization, taking advantage of the new database structure
    -Changed the logic so that chapters are referenced by their number and not by the auto-incremented document id provided by the database. EX: http://localhost:8000/chapters/1050/pages/01/ now returns the image/page '01.jpg' of the 1050th chapter

--- views.py
+++ views.py
@@ -1,55 +1,52 @@
 import json
-from pathlib import Path
+from tinydb import TinyDB, Query
 
 with open('one_piece_db.json', 'r') as f:
     data = json.load(f)
 
+INFORMATIONS = data['informations']
+CHAPTERS = data['chapters']
+db = TinyDB('one_piece_db.json', indent=4, ensure_ascii=False)
+MANGA = db.table('chapters')
+
 
 def get_about():
-    for name in data['informations']:
-        return data['informations'][name]
+    for name in INFORMATIONS:
+        return INFORMATIONS[name]
 
 
-def get_all_images():
-    IMAGES = []
-    for name in data['pages']:
-        IMAGES.extend(iter(data['pages'][name]['name']))
-    return IMAGES
+def get_all_titles():
+    return [CHAPTERS[index]['name'] for index in CHAPTERS]
 
 
-def get_specifique(id: str):
-    for _ in data['pages']:
-        return [
-            data['pages'][id]
-            if id in data['pages']
-            else {f'id {id}: not in the database'}
-        ]
+def get_specific_chapter(number: str):
+    chapter = MANGA.get(Query().number == number)
+    return chapter if chapter is not None \
+        else {f'Chapter {number} not found'}
 
 
 def get_last_chapter():
-    for _ in data['pages']:
-        return data['pages']['1']
+    return CHAPTERS['1']
 
 
-def get_last_five_chapter():
-    FIVE_CHAPTER = [data['pages'][name] for name in data['pages']]
-    return FIVE_CHAPTER[:5]
+def get_last_five_chapters():
+    return CHAPTERS[:5]
 
 
-def get_name_only(id: str):
-    for _ in data['pages']:
-        return [
-            data['pages'][id]['name']
-            if id in data['pages']
-            else {f"id {id}: not in the database"}
-        ]
+def get_title_of_chapter(number: str):
+    chapter = MANGA.get(Query().number == number)
+    return chapter['name'] if chapter is not None \
+        else {f'Chapter {number} not found'}
 
 
-def get_number_chapter_only(id: str, number_page: str):
-    new_array = []
-    [new_array.append(name) for name in data['pages'][id]['chapters']]
+def get_link_adress_of_specific_page(number: str, number_page: str):
+    chapter = MANGA.get(Query().number == number)
 
-    for names in new_array:
-        chemin = Path(names)
-        if number_page in chemin.parts[-1]:
-            return names
+    if chapter is None:
+        return {f'The chapter {number} is not in the database'}
+
+    adresses = [link_adress for link_adress in chapter['pages']]
+    for link_adress in adresses:
+        if number_page == link_adress.split("/")[-1].split('.')[0]:
+            return link_adress
+    return {f'The chapter {number} does not have a page {number_page}'}
